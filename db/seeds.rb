f6028b8ee6e0bb041da0e1ffa1515498206ad908
Vasco Oliveira <vascooliveira@outlook.com>
2020-11-04 09:11:03 +0000

    adding controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+articles = 10.times { Article.create([{ title: Faker::Music.band, content: Faker::Music.album }]) }
